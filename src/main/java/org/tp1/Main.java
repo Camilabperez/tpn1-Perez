9fe8ac2aa1c3d90200e9cb6f3014828b420d0ff2
Camilabperez <Camilabperez@hotmail.com>
2024-03-12 00:14:56 -0300

    Primer commit

---
+++ src/main/java/org/tp1/Main.java
@@ -0,0 +1,9 @@
+package org.tp1;
+import java.util.logging.Logger;
+
+public class Main {
+    static Logger logger = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        logger.info("Hello and welcome!");
+    }
+}
